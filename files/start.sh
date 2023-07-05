03cd0a99326a317006bbaae232156a9df58afa06
Aswin1118 <aswinvj390@gmail.com>
2023-07-05 18:21:46 +0530

    new

--- files/start.sh
+++ files/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-APP_JAR_FILE=/home/ubuntu/petclinic-3.0.7.jar
-AGENT_JAR_FILE=/home/ubuntu/jmx_prometheus_javaagent-0.18.0.jar
+JAR_FILE=/home/ubuntu/petclinic-3.0.7.jar
 APP_PROPERTIES=/opt/application.properties
 PROPERTIES_SCRIPT=/home/ubuntu/properties.py
 
-# to run the Python script
-sudo python3 "${PROPERTIES_SCRIPT}"
+# to run python script
+sudo python3 ${PROPERTIES_SCRIPT}
 
 # start consul
 sudo nohup consul agent -config-dir /etc/consul.d/ &
 
-# start JMX and petclinic application jar file
-sudo java -javaagent:"${AGENT_JAR_FILE}=9090:/home/ubuntu/config.yml" -jar "${APP_JAR_FILE}" --spring.config.location="${APP_PROPERTIES}" --spring.profiles.active=mysql &
+# start jmx and petclinic application jar file
+sudo java -jar ${JAR_FILE} --spring.config.location=${APP_PROPERTIES} --spring.profiles.active=mysql & \
+java -javaagent:/home/ubuntu/jmx_prometheus_javaagent-0.18.0.jar=9090:/home/ubuntu/config.yml -jar ${JAR_FILE} &
